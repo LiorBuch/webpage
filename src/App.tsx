89ae64874be78aef1250f01eac0764858b40eeb7
liorb <lior.buchmeier@gmail.com>
2023-07-09 20:25:38 +0300

    particle

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bh from './assets/bh.gif'
+import BackStar from './comps/background'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BackStar/>
+      <img src={bh} alt="BH"/>
+      <h1>Hello There!</h1>
+      <h1>site under constructions</h1>
     </div>
   );
 }
